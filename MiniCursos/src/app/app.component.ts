623961c08b7c96835e8be628b1c6cd6ec5e32dec
Livia Vitória Da Silva <20231014040029@ifrn.local>
2025-01-31 10:33:41 -0300

    :heavy_plus_sign: feat: adiciona bootstrap e card. #18

--- MiniCursos/src/app/app.component.ts
+++ MiniCursos/src/app/app.component.ts
@@ -4,8 +4,29 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  
+  <main class="main">
+  <div class="container">
+    <h1>Mini cursos</h1>
+    <div class="card mb-3">
+  <img src="../assets/fora.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+  </div>
+</div>
+   </div>
+  </main>
+  <router-outlet>
+
+`,
+
+  styles:`
+ 
+    
+  `
 })
 export class AppComponent {
   title = 'MiniCursos';
